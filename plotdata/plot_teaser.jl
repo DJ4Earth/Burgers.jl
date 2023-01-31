49df5284c610cffe2961a3ea0936723e9890a4d7
Michel Schanen <mschanen@anl.gov>
2023-01-31 13:19:07 -0600

    Fixes: Using Gaussian IC

--- plotdata/plot_teaser.jl
+++ plotdata/plot_teaser.jl
@@ -1,25 +1,60 @@
 using Plots
 using JLD
 
-default(titlefont = (14, "times"))
-color = :heat
+# default(titlefont = (14, "times"))
+color = :balance
 
-vel = load("plotdata/IC.jld", "vel")
+vel = load("IC.jld", "vel")
 senergy = sum(vel)/(size(vel, 1)*size(vel, 2))
 println("Initial energy: $senergy")
-heatmap(vel;xlabel = "x", ylabel = "y", c = color)
+surface(
+    range(-3, 3, length=size(vel,1)),
+    range(-3, 3, length=size(vel,1)),
+    vel;xlabel = "x", ylabel = "y", c = color,
+    legend=:none
+)
 savefig("plotdata/ic.pdf")
-vel = load("plotdata/final.jld", "vel")
+vel = load("final.jld", "vel")
 senergy = sum(vel)/(size(vel, 1)*size(vel, 2))
 println("Final energy: $senergy")
-heatmap(vel; xlabel = "x", ylabel = "y", c = color)
+surface(
+    range(-3, 3, length=size(vel,1)),
+    range(-3, 3, length=size(vel,1)),
+    vel;xlabel = "x", ylabel = "y", c = color,
+    legend=:none
+)
 savefig("plotdata/final.pdf")
-vel = load("plotdata/adjoints.jld", "dvel")
+vel = load("adjoints.jld", "dvel")
+du = load("adjoints.jld", "du")
+dv = load("adjoints.jld", "dv")
 maximum(vel)
 senergy = sum(vel)/(size(vel, 1)*size(vel, 2))
 println("Adjoint energy: $senergy")
-vel .= vel .* 1e6
-heatmap(vel; xlabel = "x", ylabel = "y", c = color)
+vel .= vel .* 1e4
+surface(
+    range(-3, 3, length=size(vel,1)),
+    range(-3, 3, length=size(vel,1)),
+    vel;xlabel = "x", ylabel = "y", c = color,
+    legend=:none
+)
 savefig("plotdata/dbc.pdf")
-heatmap(vel[2:end-1,2:end-1];xlabel = "x", ylabel = "y", c = color)
+surface(
+    range(-3, 3, length=size(vel,1)),
+    range(-3, 3, length=size(vel,1)),
+    du;xlabel = "x", ylabel = "y", c = color,
+    legend=:none
+)
+savefig("plotdata/du.pdf")
+surface(
+    range(-3, 3, length=size(vel,1)),
+    range(-3, 3, length=size(vel,1)),
+    dv;xlabel = "x", ylabel = "y", c = color,
+    legend=:none
+)
+savefig("plotdata/dv.pdf")
+surface(
+    range(-3, 3, length=size(vel,1)),
+    range(-3, 3, length=size(vel,1)),
+    vel;xlabel = "x", ylabel = "y", c = color
+)
 savefig("plotdata/dic.pdf")
